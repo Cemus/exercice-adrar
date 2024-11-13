210f06762b066750907d7637a37b7d585812abe2
Cemus <24947582+Cemus@users.noreply.github.com>
2024-11-13 16:53:13 +0100

    Fix template creator

--- create_template.sh
+++ create_template.sh
@@ -10,21 +10,18 @@ cd "./$nom_projet" || { echo "Échec du changement de répertoire"; exit 1; }
 
 touch index.html style.css script.js
 
-echo '<!DOCTYPE html>
+echo "<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="script.js" defer/>
     <link rel="stylesheet" href="style.css" />
-    <title>' > index.html
-
-echo "$nom_projet" >> index.html
-
-echo '</title>
+    <title>$nom_projet</title>
 </head>
 <body>
     
 </body>
-</html>' >> index.html
+</html>" >> index.html
+echo "Dossier crée avec succès"
 fi
