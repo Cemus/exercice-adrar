32aa2fa2f32cd5510c1b103bc3df6a08b8a79e09
Cemus <24947582+Cemus@users.noreply.github.com>
2024-11-13 16:21:13 +0100

    Add localstorage

--- christian/script.js
+++ christian/script.js
@@ -3,16 +3,15 @@ const input = document.getElementsByTagName("input")[0];
 const textArea = document.getElementsByTagName("textarea")[0];
 const button = document.getElementsByTagName("button")[0];
 
-button.addEventListener("click", () => {
-  input.value = "";
-  textArea.value = "";
-  message.disabled = false;
-  input.disabled = false;
-  message.classList.add("hidden");
-  button.classList.add("hidden");
-});
+const storageText = localStorage.getItem("text");
+if (storageText) {
+  localStorage.setItem("text", storageText);
+  input.value = storageText;
+  textArea.value = input.value;
+  checkInputs();
+}
 
-document.addEventListener("keyup", () => {
+function checkInputs() {
   if (textArea.value.length > 5) {
     message.classList.remove("hidden");
     button.classList.remove("hidden");
@@ -23,5 +22,17 @@ document.addEventListener("keyup", () => {
     message.disabled = false;
     input.disabled = false;
     textArea.value = input.value;
+    localStorage.setItem("text", textArea.value);
   }
+}
+
+button.addEventListener("click", () => {
+  input.value = "";
+  textArea.value = "";
+  message.disabled = false;
+  input.disabled = false;
+  message.classList.add("hidden");
+  button.classList.add("hidden");
 });
+
+document.addEventListener("keyup", checkInputs);
