f5c67147eeab07a9bc3a660b729046a0a10d1d10
Cemus <24947582+Cemus@users.noreply.github.com>
2024-11-14 11:35:46 +0100

    fix create_template

--- create_template.sh
+++ create_template.sh
@@ -12,12 +12,12 @@ cd "./$nom_projet" || { echo "Échec du changement de répertoire"; exit 1; }
 touch index.html style.css script.js
 
 echo "<!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="module" src="script.js" defer/>
-    <link rel="stylesheet" href="style.css" />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <script type='module' src='script.js' defer> </script>
+    <link rel='stylesheet' href='style.css' />
     <title>${nom_projet^}</title>
 </head>
 <body>
