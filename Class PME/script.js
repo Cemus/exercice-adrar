ab3d47349f74dab93110032f6101c793ea703424
Cemus <24947582+Cemus@users.noreply.github.com>
2024-11-25 11:30:36 +0100

    Add class PME

---
+++ Class PME/script.js
@@ -0,0 +1,49 @@
+class Employee {
+  constructor(name, surname, age, salary) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.salary = salary;
+  }
+  getMonthlySalary(N) {
+    const result = this.salary * N;
+    return result * 1.9;
+  }
+}
+
+class Pme {
+  constructor(name, team, R, FF, FA, N) {
+    this.name = name;
+    this.team = team;
+    this.R = R;
+    this.FF = FF;
+    this.FA = FA;
+    this.N = N;
+  }
+  getHumanCost() {
+    let result = 0;
+    for (let index = 0; index < this.team.length; index++) {
+      const element = this.team[index];
+      result += element.getMonthlySalary(this.N);
+    }
+    return result;
+  }
+  getEconomicBalance() {
+    return this.getHumanCost() + this.R + this.FF + this.FA;
+  }
+}
+
+const myPme = new Pme(
+  "Ma Petite Entreprise - ",
+  [
+    new Employee("Duval", "Paul", 30, 2000),
+    new Employee("Durand", "Alain", 40, 3000),
+    new Employee("Dois", "Sylvia", 50, 4000),
+  ],
+  300000,
+  20000,
+  50000,
+  12
+);
+
+console.log("BILAN :", myPme.getEconomicBalance());
