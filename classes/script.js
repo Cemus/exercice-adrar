2e7f9f0ddfbaae5b42af10e596e9a4007e0fea2b
Cemus <24947582+Cemus@users.noreply.github.com>
2024-11-25 11:08:37 +0100

    Ui

--- classes/script.js
+++ classes/script.js
@@ -9,6 +9,7 @@ class Imc {
     this.personName = personName;
     this.mass = mass;
     this.height = height;
+    this.imc = null;
   }
   calculateImc() {
     return (this.mass / this.height ** 2).toFixed(2);
@@ -19,32 +20,32 @@ class Imc {
    * @returns
    */
   getStatus(imc) {
+    let result = "";
     if (imc < 18.5) {
-      return "Maigreur";
+      result = "Maigreur";
     }
     if (imc >= 18.5 && imc <= 25) {
-      return "Normal";
+      result = "Normal";
     }
     if (imc > 25 && imc <= 30) {
-      return "Surpoids";
+      result = "Surpoids";
     }
     if (imc > 30 && imc <= 35) {
-      return "Obésité modérée";
+      result = "Obésité modérée";
     }
     if (imc > 35 && imc <= 40) {
-      return "Obésité sévère";
+      result = "Obésité sévère";
     }
     if (imc > 40) {
-      return "Obésité morbide";
+      result = "Obésité morbide";
     }
+    return result;
   }
   display() {
-    const imc = this.calculateImc();
-    console.log(
-      `${this.personName} (${this.mass} kg, ${
-        this.height
-      } M) a un IMC de : ${imc}\nStatut : ${this.getStatus(imc)}`
-    );
+    this.imc = this.calculateImc();
+    const result = `${this.personName} (${this.mass} kg, ${this.height} M) a un IMC de : ${this.imc}.`;
+    console.log(result);
+    return result;
   }
 }
 
@@ -60,7 +61,23 @@ const list = [
   new Imc("Harold Valise", 135, 1.5),
 ];
 
-for (let index = 0; index < list.length; index++) {
+/* for (let index = 0; index < list.length; index++) {
   const element = list[index];
   element.display();
-}
+} */
+const personName = document.getElementById("name");
+const mass = document.getElementById("mass");
+const height = document.getElementById("height");
+const button = document.getElementById("button");
+const imc = document.getElementById("imc");
+const result = document.getElementById("result");
+
+button.addEventListener("click", () => {
+  if (mass.value && personName.value && height.value) {
+    const user = new Imc(personName.value, mass.value, height.value);
+    imc.textContent = user.display();
+    result.textContent = user.getStatus(user.imc);
+  } else {
+    imc.textContent = "Not enough data";
+  }
+});
